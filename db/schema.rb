504e5b35bed683cf88db721a7e41b695321c6986
westondombroski <westondombroski@gmail.com>
2017-03-14 14:51:30 -0700

    Cities MVC and Routing, basic contoller methods for Cities.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314193323) do
+ActiveRecord::Schema.define(version: 20170314204610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
