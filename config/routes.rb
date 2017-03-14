3cd7fe342a371dd1867882ed29337b6c1dae6348
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-03-14 16:33:07 -0700

    Get started with application layout

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # User routes
   get '/users', to: 'users#index'
   post '/users', to: 'users#create'
-  get '/signup', to: 'users#new', as: 'new_user'
+  get '/signup', to: 'users#new'
   get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
   get '/users/:id', to: 'users#show', as: 'user'
   patch '/users/:id', to: 'users#update'
