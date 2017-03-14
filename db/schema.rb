a98c318dd5e083167614db5e71b265df537890b7
chri-roch12 <chris.prochnow@gmail.com>
2017-03-14 12:35:40 -0700

    Removed join_date from users table in separate migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314184922) do
+ActiveRecord::Schema.define(version: 20170314193323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20170314184922) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "current_city"
-    t.datetime "join_date"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
