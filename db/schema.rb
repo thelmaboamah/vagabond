5edc99b18628b4678d5a44539120ff491f1df770
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-03-14 20:49:29 -0700

    users#index + show inital layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314204610) do
+ActiveRecord::Schema.define(version: 20170314224434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20170314204610) do
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.string   "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
