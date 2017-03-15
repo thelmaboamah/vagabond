c42ab8a3610f94c54963cb2fdcc6ef20b201e147
westondombroski <westondombroski@gmail.com>
2017-03-15 12:54:12 -0700

    paperclip for user photo upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
-  
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -39,11 +39,11 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-  
+
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :current_city, :password,:password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :current_city, :password, :password_confirmation, :image)
     end
 
     #Confirms a logged in user
@@ -53,7 +53,7 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-    
+
 
     def correct_user
       @user = User.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170315081541) do
+ActiveRecord::Schema.define(version: 20170315193713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +25,12 @@ ActiveRecord::Schema.define(version: 20170315081541) do
   create_table "entries", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.integer  "user_id"
     t.integer  "city_id"
     t.index ["city_id"], name: "index_entries_on_city_id", using: :btree
@@ -41,9 +44,13 @@ ActiveRecord::Schema.define(version: 20170315081541) do
     t.string   "current_city"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.boolean  "admin",           default: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.boolean  "admin",              default: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_foreign_key "entries", "cities"
