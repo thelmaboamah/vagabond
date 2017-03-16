2ac6a0d368eea4fa65c064968c2c3ecb05d74661
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-03-16 11:31:34 -0700

    added toggle img

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,12 +25,22 @@
 // });
 
 $(document).ready(function(){
-	$(".img-div").on('click', function() {
-		$(".img-div").css("background-image",
-		"url(http://www.premiumtours.ca/upimg/groupe/jour/526__g958_20160907_164957-0.jpg)")
-	});
-	$(".img-div").on('click', function() {
-		$(".img-div").css("background-image",
-		"url(/assests/images/gb.jpg)")
-	});
+	cycleBackgrounds();
+
 });
+
+function cycleBackgrounds() {
+    var index = 0;
+
+    $imageEls = $('.toggle-image'); // Get the images to be cycled.
+
+    setInterval(function () {
+        // Get the next index.  If at end, restart to the beginning.
+        index = index + 1 < $imageEls.length ? index + 1 : 0;
+        // Show the next image.
+        $imageEls.eq(index).addClass('show');
+        // Hide the previous image.
+        $imageEls.eq(index - 1).removeClass('show');
+
+    }, 2000);
+};
