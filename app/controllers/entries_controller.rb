e13d99559b29adc0a1564f58d375e4a51608aa55
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-03-16 04:18:31 -0700

    Correct entries controller actions for create and update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
-  before_action :logged_in?, only: [:create, :destroy]
-  before_action :correct_user,   only: :destroy
+  before_action :logged_in?, only: [:create, :edit, :destroy]
+  before_action :correct_user,   only: [ :edit, :destroy]
 
   def index
     @entries = Entry.paginate(page: params[:page], per_page: 6)
@@ -16,11 +16,27 @@ class EntriesController < ApplicationController
 
   def create
     @entry = current_user.entries.build(entry_params)
+    if @entry.save
+      flash[:success] = "Entry created!"
+      redirect_to root_url
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
   end
 
   def update
-    @entry.update_attributes(entry_params)
-    redirect_to root_path
+    @entry = Entry.find(params[:id])
+    if @entry.update_attributes(entry_params)
+      flash[:success] = "Profile updated"
+      redirect_to entry_show_path
+    else
+      flash[:error]
+      render 'edit'
+    end
   end
 
   def destroy
